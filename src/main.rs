44cc5b382df2867b3e8f050459990c1f6b1df836
graphffiti <graphffiti@gmail.com>
2022-04-17 21:24:09 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_files::NamedFile;
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    NamedFile::open_async("resources/static/index.html").await
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
