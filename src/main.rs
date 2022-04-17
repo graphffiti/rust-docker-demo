30024de34afc7e787256077d049dd4f1c98684d2
graphffiti <graphffiti@gmail.com>
2022-04-17 21:52:08 +0800

    Changed port env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_files::NamedFile;
 use actix_web::{get, App, HttpServer, Responder};
+use std::env;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -8,8 +9,15 @@ async fn index() -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
+    println!("Port: {}", port);
+
     HttpServer::new(|| App::new().service(index))
-        .bind(("0.0.0.0", 8081))?
+        .bind(("0.0.0.0", port))?
         .run()
         .await
 }
